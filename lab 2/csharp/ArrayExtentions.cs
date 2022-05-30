a3a26e1dff13cb4c44742e4a2d887d0c6c655b8b
Nazar <77193931+KrishaPoehala@users.noreply.github.com>
2022-05-30 16:03:00 +0300

    Add files via upload

---
+++ lab 2/csharp/ArrayExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+#nullable disable
+namespace lab3;
+
+public static class ArrayExtentions
+{
+    public static string ExtractString(this int[] array)
+    {
+        var sb = new StringBuilder(array.Length);
+        foreach (var item in array)
+        {
+            sb.Append(item);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ lab 2/csharp/Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+#nullable disable
+namespace lab3;
+
+public class Client
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public Client()
+    {
+
+    }
+    public static IEnumerable<Client> GetTestSamples(int count)
+    {
+        var list = new List<Client>();
+        for (int i = 0; i < count; i++)
+        {
+            var client = new Client()
+            {
+                Name = Guid.NewGuid().ToString()[0..5],
+                SurName = Guid.NewGuid().ToString()[0..5],
+                Address = Guid.NewGuid().ToString()[0..5],
+                PhoneNumber = GetFormattedPhoneNumber(),
+            };
+
+            list.Add(client);
+        }
+
+        return list;
+    }
+
+    private static string GetFormattedPhoneNumber()
+    {
+        var numbers = Enumerable.
+                      Range(0, 10).
+                      Select(_ => Random.Shared.Next(0, 9)).
+                      ToArray();
+
+        return $"{numbers[0..3].ExtractString()}" +
+            $"-{numbers[3..6].ExtractString()}" +
+            $"-{numbers[6..8].ExtractString()}" +
+            $"-{numbers[8..10].ExtractString()}";
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} lives at {Address} with phone number {PhoneNumber}";
+    }
+
+    public int GetPhoneNumberDigitsSum() =>
+        PhoneNumber
+        .Where(x => char.IsDigit(x))
+        .Select(x => int.Parse(x.ToString()))
+        .Sum();
+        
+
+
+    
+}
