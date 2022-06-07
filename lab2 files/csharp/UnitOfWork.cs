b1b7ca94950581c1413d71fb495df21350caeb18
Nazar <77193931+KrishaPoehala@users.noreply.github.com>
2022-06-07 11:14:38 +0300

    Add files via upload

---
+++ lab2 files/csharp/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2Files;
+
+public class UnitOfWork
+{
+    public string Name { get; set; }
+    public DateTime Start { get; set; }
+    public TimeSpan Duration { get; set; }
+
+    public DateTime End() => Start + Duration;
+    public UnitOfWork(string name, DateTime start, TimeSpan duration)
+    {
+        Name = name;
+        Start = start;
+        Duration = duration;
+    }
+
+    public UnitOfWork()
+    {
+    }
+
+    public static IEnumerable<UnitOfWork> GetSamples(int count)
+    {
+        for (int i = 1; i <= count; i++)
+        {
+            var unit = new UnitOfWork()
+            {
+                Name = Guid.NewGuid().ToString(),
+                Start = DateTime.Now + TimeSpan.FromHours(2 * i),
+                Duration = TimeSpan.FromHours(1.5),
+            };
+
+            yield return unit;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Task:: {Start} - {End()}";
+    }
+}
