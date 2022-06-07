c042a37ea6ca9ef10d0ca21ea9a47c9dd2cfe53e
Nazar <77193931+KrishaPoehala@users.noreply.github.com>
2022-06-07 11:21:45 +0300

    Create program.cpp

---
+++ lab6/program.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <map>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct Node
+{
+	int key;
+	Node* left;
+	Node* right;
+};
+
